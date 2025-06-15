623a253a70abd21eb1e1957638ff2354d08d914a
Aarav Eeshan <aaraveeshan03@gmail.com>
2025-06-16 02:21:57 +0530

    fixing

--- main_query.py
+++ main_query.py
@@ -1,14 +1,10 @@
 from components.query_engine import QueryEngine
 from components.response_generator import ResponseGenerator
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
 model_embed = "text-embedding-3-large"
 model_chat = "gpt-4"
 
-def main_query(query):
+def main_query(query, api_key):
     chunks_obj = QueryEngine(model_embed, api_key)
     context_chunks = chunks_obj.search(query)
     response_obj = ResponseGenerator(model_chat, api_key)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from main_query import main_query
 
+api_key = st.secrets["OPENAI_API_KEY"]
 st.set_page_config(page_title="COS Chatbot", layout="wide")
 st.title("📄 IITD Courses of Study Chatbot")
 st.markdown("Ask a question based on the Courses Of Study of IITD.")
@@ -9,7 +10,7 @@ query = st.text_input("🔍 Your Question")
 
 if query:
     with st.spinner("Thinking..."):
-        answer = main_query(query)
+        answer = main_query(query, api_key)
 
     st.success("✅ Answer:")
     st.markdown(answer)
